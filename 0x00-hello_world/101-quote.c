0dfec6d0614e38a325e3126096e0ba2555c2ad8c
nohzeeM <nohzeematterfact@gmail.com>
2023-09-07 13:17:55 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: 0
+ */
+int main(void)
+{
+	fwrite(2, "and that piece of art is useful\" . Dora Korpar, 2015-10-19\n", 59;
+	return (1);
+}
