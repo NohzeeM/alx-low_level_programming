0cb5f932cf6f39e66dfbf2d00cbacaec5e4550e1
nohzeeM <nohzeematterfact@gmail.com>
2023-09-13 07:58:46 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: 0
+ */
+int main(void)
+{
+print_times_table(3);
+_putchar('\n');
+print_times(5);
+print_times(98);
+_putchar('\n');
+print_times_table(12);
+return (0);
+}
